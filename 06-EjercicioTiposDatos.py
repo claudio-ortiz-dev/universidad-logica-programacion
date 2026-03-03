e2bb6ae1cda69feddc5288d35554688b2c5be81c
claudio <claudio.ortiz.dev@gmail.com>
2026-03-02 22:00:25 -0800

    Ejercicio 6

---
+++ 06-EjercicioTiposDatos.py
@@ -0,0 +1,13 @@
+nombre = input("Proporciona tu nombre: ")
+
+edad = int(input("Proporciona tu edad: "))
+
+sueldo = float(input("Proporciona tu sueldo (USD): "))
+
+empleado = input("Eres empleado de confianza (True/False): ")
+
+print("\nLa información proporcionada es:")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Sueldo:", sueldo)
+print("Empleado de confianza:", empleado)
