a7ee7e5ab17aa207a336837bd031c49ffc87acc8
claudio <claudio.ortiz.dev@gmail.com>
2026-03-02 22:41:28 -0800

    Ignorar .DS_Store

---
+++ constantes.py
@@ -0,0 +1,18 @@
+# Variable normal
+mi_variable = 10
+print(mi_variable)
+
+mi_variable = 15
+print(mi_variable)
+
+# Constante matemática PI
+import math
+print(math.pi)
+
+# Constante por convención
+SEGUNDOS_POR_MINUTO = 60
+print(SEGUNDOS_POR_MINUTO)
+
+# No deberíamos modificarla, pero Python lo permite
+# SEGUNDOS_POR_MINUTO = 70
+# print(SEGUNDOS_POR_MINUTO)
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+# Operadores Aritmeticos
+
+# Suma +
+a = 3 + 4
+print("Suma:", a)
+
+# Resta -
+b = 6 - 2
+print("Resta:", b)
+
+# Multiplicacion *
+c = a * 2
+print("Multiplicacion:", c)
+
+# Division /
+d = b / c
+print("Division:", d)
+
+# Potencia **  (NO es ^ en Python)
+e = a ** 2
+print("Potencia:", e)
+
+# Modulo %
+f = 9 % 3
+print("Modulo:", f)
